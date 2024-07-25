e24e687891db2782d2bf1812f7765fb75850ce24
Leovhernandez <ninihernandez@hotmail.com>
2024-07-24 21:08:41 -0500

    db connection pool functionality added

--- database_connection.py
+++ database_connection.py
@@ -1,7 +1,7 @@
 import os
 import psycopg2
 import configparser
-
+from psycopg2 import pool
 
 # Load connection parameters prioritizing environment variables first then 'config.ini' being the fallback
 # ***Ensure that your 'config.ini' file is added to .gitignore so it is NOT tracked by GIT
@@ -15,26 +15,36 @@ params = {
     'port': os.getenv('DB_PORT', config.get('database', 'port')) # Default PostgreSQL port
 }
 
-# Try establishing the connection using the unpacking arguments operator **
-# Will return the conn connection object if successful
+# Setting up connection pool which is more efficient for repeated database interactions instead of direct connection
+# SimpleConnectionPool(1, 10, **params) opens the connection pool with a min of 1 connection and max of 10 connections
+db_conn_pool = psycopg2.pool.SimpleConnectionPool(1, 10, **params)
+
 try:
-    conn = psycopg2.connect(**params)
-    print("Connection to the database successful")
+    # Try to get a connection from the pool
+    conn = db_conn_pool.getconn()
     
-    # Create a cursor object
-    cursor = conn.cursor()
+    # if conn is true (successful) 
+    if conn:
+        print("Connection to the database successful")
     
-    # Execute a query
-    cursor.execute("SELECT version();")
+        # Create a cursor object
+        cursor = conn.cursor()
     
-    # Fetch result
-    record = cursor.fetchone()
-    print(f"You are connected to - {record[0]}")
+        # Execute a query
+        cursor.execute("SELECT version();")
     
-    # Close the cursor and the connection
-    cursor.close()
-    conn.close()
+        # Fetch result
+        record = cursor.fetchone()
+        print(f"You are connected to - {record[0]}")
+        
+        # Close the cursor and the connection
+        cursor.close()
+        conn.close() # Actually returns the connection to the pool
+        db_conn_pool.putconn(conn) # Explicitly return the connection to the pool
 
 except (Exception, psycopg2.DatabaseError) as error:
     print("Error while connecting to PostgreSQL", error)
-    
+finally:
+    # Close the connection pool
+    if db_conn_pool:
+        db_conn_pool.closeall()
